920bb7bd4c9c4eed1dcc0f76453265015e3a502e
youngwoo <duddn520@naver.com>
2022-03-17 15:56:55 +0900

    Initial commit

---
+++ src/main/java/com/helloworld/helloworldweb/HelloworldwebApplication.java
@@ -0,0 +1,13 @@
+package com.helloworld.helloworldweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helloworld/helloworldweb/HelloworldwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworld.helloworldweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
