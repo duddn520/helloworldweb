4f0b20ff28b9bc239ab830834ef8fdfdbb730547
duddn520 <duddn520@konkuk.ac.kr>
2022-03-25 18:10:10 +0900

    add - securityautoconfig exclude

--- src/main/java/com/helloworld/helloworldweb/HelloworldwebApplication.java
+++ src/main/java/com/helloworld/helloworldweb/HelloworldwebApplication.java
@@ -2,8 +2,9 @@ package com.helloworld.helloworldweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class HelloworldwebApplication {
 
 	public static void main(String[] args) {
