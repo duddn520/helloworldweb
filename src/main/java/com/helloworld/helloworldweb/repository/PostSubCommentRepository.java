e0e958c4269f2205be3de3229e2d5fc112a6da0e
duddn520 <duddn520@konkuk.ac.kr>
2022-03-31 20:24:51 +0900

    add - PostSubCOmment CRUD 생성.

--- src/main/java/com/helloworld/helloworldweb/repository/PostSubCommentRepository.java
+++ src/main/java/com/helloworld/helloworldweb/repository/PostSubCommentRepository.java
@@ -3,5 +3,8 @@ package com.helloworld.helloworldweb.repository;
 import com.helloworld.helloworldweb.domain.PostSubComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PostSubCommentRepository extends JpaRepository<PostSubComment,Long> {
+    Optional<PostSubComment> findById(Long id);
 }
---
+++ src/main/java/com/helloworld/helloworldweb/service/PostSubCommentService.java
@@ -0,0 +1,33 @@
+package com.helloworld.helloworldweb.service;
+
+import com.helloworld.helloworldweb.domain.PostComment;
+import com.helloworld.helloworldweb.domain.PostSubComment;
+import com.helloworld.helloworldweb.repository.PostSubCommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PostSubCommentService {
+
+    private final PostSubCommentRepository postSubCommentRepository;
+
+    public PostSubComment getPostSubCommentById(Long id)
+    {
+        return postSubCommentRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 대댓글이 존재하지 않습니다."));
+    }
+
+    @Transactional
+    public PostSubComment registerPostSubComment(PostSubComment postSubComment, PostComment postComment)
+    {
+        postSubComment.updatePostComment(postComment);
+        return postSubCommentRepository.save(postSubComment);
+    }
+
+    @Transactional
+    public void deletePostSubComment(PostSubComment postSubComment)
+    {
+        postSubCommentRepository.delete(postSubComment);
+    }
+}
