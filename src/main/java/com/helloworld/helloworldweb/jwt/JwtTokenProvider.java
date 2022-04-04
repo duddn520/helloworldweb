76328bfcbc25f7be450ab12e5471ff56d7f55b3f
duddn520 <duddn520@konkuk.ac.kr>
2022-04-04 22:27:57 +0900

    add - jwt 생성 컴포넌트, Authentication 관련 컴포넌트 추가.

---
+++ src/main/java/com/helloworld/helloworldweb/jwt/JwtTokenProvider.java
@@ -0,0 +1,79 @@
+package com.helloworld.helloworldweb.jwt;
+
+import com.helloworld.helloworldweb.domain.Role;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Component
+public class JwtTokenProvider {
+
+    private final UserDetailsService userDetailsService;
+
+    private String secretKey = "HELLO_WORLD_WEB";
+
+    private static final long TOKEN_VALID_TIME = 1000L * 60 * 60 * 10; //10시간
+
+    @PostConstruct
+    protected void init(){
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    //JWT토큰생성
+    public String createToken(String email, Role role) {
+        Claims claims = Jwts.claims().setSubject(email);
+        claims.put("role", role);
+        Date now = new Date();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + TOKEN_VALID_TIME))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+
+    }
+
+    public Authentication getAuthentication(String token){
+        UserDetails userDetails = userDetailsService.loadUserByUsername(getUserEmail(token));
+        return new UsernamePasswordAuthenticationToken(userDetails,"",userDetails.getAuthorities());
+    }
+
+    public String getUserEmail(String token)
+    {
+        return Jwts.parser().setSigningKey(secretKey)
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean verifyToken(String jwtToken){
+        try{
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Object getRole(String token){
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().get("role");
+    }
+
+    public String getTokenByHeader(HttpServletRequest request) {
+        return request.getHeader("Auth");
+    }
+
+
+
+}
