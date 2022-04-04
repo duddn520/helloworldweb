19277283d0dbc3b156b22f622be2908f23d226dc
duddn520 <duddn520@konkuk.ac.kr>
2022-04-04 22:28:28 +0900

    add - 커스텀 필터 생성, 필터체인 추가.

---
+++ src/main/java/com/helloworld/helloworldweb/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,36 @@
+package com.helloworld.helloworldweb.jwt;
+
+import com.helloworld.helloworldweb.domain.Role;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends GenericFilterBean {
+
+    private JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        String token = ((HttpServletRequest)request).getHeader("Auth");
+        response.setCharacterEncoding("UTF-8");
+
+        if (token != null && jwtTokenProvider.verifyToken(token)) {     //토큰 유효성 검증 및 auth객체 생성 후 SecurityContextHolder에 등록.
+            Role role =  Role.valueOf((String)jwtTokenProvider.getRole(token));
+            Authentication auth = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        chain.doFilter(request, response);
+    }
+}
