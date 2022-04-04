a6f23f91e5002c8c39efc2f0505aca6ded098cc4
duddn520 <duddn520@konkuk.ac.kr>
2022-04-04 22:26:03 +0900

    fix - (exclude = SecurityAutoConfiguration.class) 제거.

--- src/main/java/com/helloworld/helloworldweb/HelloworldwebApplication.java
+++ src/main/java/com/helloworld/helloworldweb/HelloworldwebApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
+//@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class HelloworldwebApplication {
 
 	public static void main(String[] args) {
