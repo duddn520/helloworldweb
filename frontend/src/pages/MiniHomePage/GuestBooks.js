a3444a711dd047788193a65cd0dd12d2f04a7646
HEO JIHUN(MAC) <ys05143@gmail.com>
2022-04-22 15:50:15 +0900

    fix - 네이버 로그인 방법 변경 (서버에서 AccessToken 가져오는 방식으로 변경) add - 미니홈페이지 틀(가이드 라인), 네이버 로그인 redirect add - frontend에 네이버 로그인 추가

---
+++ frontend/src/pages/MiniHomePage/GuestBooks.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function GuestBooks(){
+    return(
+        <div>
+            <h1>GuestBooks</h1>
+        </div>
+    )
+}
+
+export default GuestBooks;
---
+++ frontend/src/pages/MiniHomePage/MyInfo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MyInfo(){
+    return(
+        <div>
+            <h1>MyInfo</h1>
+        </div>
+    )
+}
+
+export default MyInfo;
