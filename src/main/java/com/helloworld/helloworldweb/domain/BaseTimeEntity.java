a021ace9e0992bf3190f0f304522a151ac096233
jinwoo <wlsdn1372@naver.com>
2022-04-28 17:29:31 +0900

    add - [FE]GuestBooks.js - 방명록 탭 추가 update - [FE] Main.js - 메인화면 변경 add - 방명록 조회,수정 기능 add - qna 등록,조회

---
+++ src/main/java/com/helloworld/helloworldweb/domain/BaseTimeEntity.java
@@ -0,0 +1,20 @@
+package com.helloworld.helloworldweb.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdTime;
+
+
+}
