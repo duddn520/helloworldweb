903f7abf3c5ee5e575ff298766514584a1f8eee8
JiHun Heo <77225982+ys05143@users.noreply.github.com>
2022-05-18 19:44:47 +0900

    refactor - refreshToken으로 accessToken 재발급 수정

--- frontend/src/api/request.js
+++ frontend/src/api/request.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 import api from "./api";
-
+import { useNavigate } from 'react-router-dom';
 
 const request = axios.create({
     timeout: 20000
 })
-
-
 
 // HTTP 요청 직전에 가로챔. ( 전역 )
 // jwt 있을 시 넣어서 요청 
@@ -25,22 +23,24 @@ request.interceptors.request.use(
     }
 );
 
-// 
 request.interceptors.response.use(
     (response) => {
         return response;
     },
     (error) => {
+        
         // 서버 에러가 난 경우
         if( error.response.status === 500 && hasToken() ){
             api.getNewToken( window.sessionStorage.getItem("Auth") , window.sessionStorage.getItem("Refresh") )
             .then( res => {
-                if( res.Auth )
-                    window.sessionStorage.setItem("Auth",res.Auth);
-                alert("다시 시도해주세요.");
+                if( res.auth )
+                    window.sessionStorage.setItem("Auth",res.auth);
+                window.location.reload();
             })
             .catch( e => {
-                alert("다시 로그인 해주세요.")
+                // alert("다시 로그인 해주세요.")
+                window.sessionStorage.clear();
+                window.location.replace("/login");
             })
         }
         return Promise.reject(error);
