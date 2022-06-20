309231a80d5159c6c1cd7b62acf696d3cf3967e7
duddn520 <duddn520@konkuk.ac.kr>
2022-06-20 15:46:10 +0900

    add - 알림 마무리 & github 연동/ 로그인 안되는 경우 fix

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -14,5 +14,18 @@ firebase.initializeApp({
     measurementId: "G-WY2EN2Z59J"
   });
 
-  const messaging = firebase.messaging();
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Received background message ', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
   
