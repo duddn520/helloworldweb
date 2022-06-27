859ceaa66b34eea5e6d94929d40616b4bd6eaa56
duddn520 <duddn520@konkuk.ac.kr>
2022-06-27 12:31:00 +0900

    fix - docker 배포용 url 바꾸기 (localhost -> serverurl)

--- frontend/src/api/URL.js
+++ frontend/src/api/URL.js
@@ -1,8 +1,8 @@
 const urls = {
-    server: "http://localhost:8080",
-    front: "http://localhost:3000"
-    // server: "http://3.35.188.47:8080",
-    // front: "http://3.35.188.47"
+    // server: "http://localhost:8080",
+    // front: "http://localhost:3000"
+    server: "http://3.35.188.47:8080",
+    front: "http://3.35.188.47"
 }
 
 export default urls;
