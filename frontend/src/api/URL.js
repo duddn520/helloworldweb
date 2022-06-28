b45b2a827d4813591da7b902bea24236b114bdba
duddn520 <duddn520@konkuk.ac.kr>
2022-06-28 17:36:25 +0900

    fix - 새 서버용 url 변경.

--- frontend/src/api/URL.js
+++ frontend/src/api/URL.js
@@ -1,8 +1,8 @@
 const urls = {
     // server: "http://localhost:8080",
     // front: "http://localhost:3000"
-    server: "http://3.35.188.47:8080",
-    front: "http://3.35.188.47"
+    server: "http://3.37.52.109:8080",
+    front: "http://3.37.52.109"
 }
 
 export default urls;
